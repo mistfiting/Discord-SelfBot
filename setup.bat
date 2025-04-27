44a67a749778ff5f4529f3267f7bba4e7150d135
mistfiting <larrybtw716@gmail.com>
2025-04-27 14:16:16 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,6 +3,3 @@ cls
 echo python main.py >> start.bat
 start start.bat
 start /b "" cmd /c &exit /b
-$ git clone https://github.com/AstraaDev/Discord-SelfBot.git
-$ python -m pip install -r requirements.txt
-$ python main.py
